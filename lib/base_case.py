a071ac613d8a2c0cc3fc274f0a30a495dec4eec6
nkazakova <nlkazakova9@gmail.com>
2022-01-07 23:41:20 +0300

    logging

--- lib/base_case.py
+++ lib/base_case.py
@@ -1,4 +1,5 @@
 import json.decoder
+from datetime import datetime
 
 from requests import Response
 
@@ -16,7 +17,19 @@ class BaseCase:
             response_as_dict = response.json()
         except json.decoder.JSONDecodeError:
             assert False, f"Response is not in JSON format. Response text is '{response.text}'"
-
         assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
-
         return response_as_dict[name]
+
+    def prepare_registration_data(self, email=None):
+        if email is None:
+            base_part = "learnqa"
+            domain = "example.com"
+            random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+            email = f"{base_part}{random_part}@{domain}"
+        return {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnwa',
+            'email': email
+        }
